33d4369b6c0a747351ce74ad80e424ef45dd4050
Josh Neta <joshneta.github@gmail.com>
2017-11-08 11:54:28 -0600

    Loading from byte array Load empty Initial version of Sprite to byte array

--- SpriteLibrary.Tests/SpriteTests.cs
+++ SpriteLibrary.Tests/SpriteTests.cs
@@ -10,15 +10,61 @@ namespace SpriteLibrary.Tests
         public void should_throw_exception_when_sprite_doesnt_start_with_ZSPR()
         {
             byte[] testSprite = { (byte)'X', (byte)'S', (byte)'P', (byte)'R' };
-            Assert.Throws<Exception>(() => { var t = new Sprite(testSprite); });
+            Assert.Throws<Exception>(() => { var s = new Sprite(testSprite); });
         }
 
         [Fact]
-        public void should_create_new_sprite()
+        public void should_throw_exception_when_sprite_byte_array_is_too_short()
         {
             byte[] testSprite = { (byte)'Z', (byte)'S', (byte)'P', (byte)'R' };
-            var t = new Sprite(testSprite);
-            Assert.IsType<Sprite>(t);
+            Assert.Throws<Exception>(() => { var s = new Sprite(testSprite); });
+        }
+
+        [Fact]
+        public void should_create_new_sprite_from_byte_array()
+        {
+            byte[] testSprite = { (byte)'Z', (byte)'S', (byte)'P', (byte)'R', // header
+                                    1, // version
+                                    0x00, 0x00, 0xFF, 0xFF, // checksum
+                                    0x25, 0x00, 0x00, 0x00, // pixel offset
+                                    0x01, 0x00, // pixel length
+                                    0x26, 0x00, 0x00, 0x00, // palette offset
+                                    0x01, 0x00, // palette length
+                                    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // reserved
+                                    0x65, 0x00, 0x00, 0x00, // display text
+                                    0x41, 0x00, 0x00, 0x00, // author
+                                    // pixels
+                                    0x13,
+                                    // palette
+                                    0x31, 
+                                };
+            var s = new Sprite(testSprite);
+            Assert.IsType<Sprite>(s);
+            Assert.Equal(0x13, s.PixelData[0]);
+            Assert.Equal(0x31, s.PaletteData[0]);
+            Assert.Equal("e", s.DisplayText);
+            Assert.Equal("A", s.Author);
+        }
+
+        [Fact]
+        public void should_create_empty_sprite_with_version_1()
+        {
+            var s = new Sprite();
+            Assert.Equal(1, s.Version);
+        }
+        
+        [Fact]
+        public void should_create_empty_sprite_with_display_text_Unknown()
+        {
+            var s = new Sprite();
+            Assert.Equal("Unknown", s.DisplayText);
+        }
+
+        [Fact]
+        public void should_create_empty_sprite_with_author_Unknown()
+        {
+            var s = new Sprite();
+            Assert.Equal("Unknown", s.Author);
         }
     }
 }
--- SpriteLibrary/Sprite.cs
+++ SpriteLibrary/Sprite.cs
@@ -11,7 +11,7 @@ namespace SpriteLibrary
         /*
 header flag (4 bytes) = ZSPR
 version (1 byte)
-checksum (2 byte)
+checksum (4 byte)
 sprite data offset (4 bytes)
 sprite data length (2 bytes)
 palette data offset (4 bytes)
@@ -22,51 +22,176 @@ author (x bytes) (null terminated)
 sprite data (0x7000 bytes)
 palette data (0x78 + ? bytes) (remember to add extra bytes for gloves)
          */
-        // 0-3
+
         public string Header { get; private set; } = "ZSPR";
         const int headerOffset = 0;
         const int headerLength = 4;
-        // 4
+
         public byte Version { get; private set; } = 1;
         const int versionOffset = headerOffset + headerLength;
         const int versionLength = 1;
-        // 5
-        public byte CheckSum { get; private set; }
+        const int currentVersion = 1;
+
+        public uint CheckSum { get; private set; }
         const int checksumOffset = versionOffset + versionLength;
-        const int checksumLength = 1;
-        // 6-9
-        public int SpriteDataOffset { get; private set; }
+        const int checksumLength = 4;
+
+        public uint SpriteDataOffset { get; private set; }
         const int spriteDataOffsetOffset = checksumOffset + checksumLength;
         const int spriteDataOffsetLength = 4;
-        // 10-11
-        public int SpriteDataLength { get; private set; }
+
+        public ushort SpriteDataLength { get; private set; }
         const int spriteDataLengthOffset = spriteDataOffsetOffset + spriteDataOffsetLength;
         const int spriteDataLengthLength = 2;
-        // 12-15
-        public int PaletteDataOffset { get; private set; }
+
+        public uint PaletteDataOffset { get; private set; }
         const int paletteDataOffsetOffset = spriteDataLengthOffset + spriteDataLengthLength;
         const int paletteDataOffsetLength = 4;
-        // 16-17
-        public int PaletteDataLength { get; private set; }
+
+        public ushort PaletteDataLength { get; private set; }
         const int paletteDataLengthOffset = paletteDataOffsetOffset + paletteDataOffsetLength;
         const int paletteDataLengthLength = 2;
-        public byte[] Reserved { get; private set; } = new byte[8];
+
+        public byte[] Reserved { get; private set; } = new byte[reservedLength];
         const int reservedOffset = paletteDataLengthOffset + paletteDataLengthLength;
         const int reservedLength = 8;
-        public string DisplayText { get; set; }
-        const int displayTextOffset = reservedOffset + reservedLength;
-        public string Author { get; set; }
+
+        string displayText;
+        byte[] displayBytes;
+        public string DisplayText
+        {
+            get { return displayText; }
+            set
+            {
+                displayText = value;
+                displayBytes = Encoding.Unicode.GetBytes(displayText + '\0');
+                displayBytesLength = (uint)displayBytes.Length;
+
+                SpriteDataOffset = displayTextOffset + displayBytesLength + authorBytesLength;
+                PaletteDataOffset = SpriteDataOffset + SpriteDataLength;
+            }
+        }
+        const uint displayTextOffset = reservedOffset + reservedLength;
+        uint displayBytesLength = 0;
+
+        string author;
+        byte[] authorBytes;
+        public string Author
+        {
+            get { return author; }
+            set
+            {
+                author = value;
+                authorBytes = Encoding.Unicode.GetBytes(author + '\0');
+                authorBytesLength = (uint)authorBytes.Length;
+
+                SpriteDataOffset = displayTextOffset + displayBytesLength + authorBytesLength;
+                PaletteDataOffset = SpriteDataOffset + SpriteDataLength;
+            }
+        }
+        uint authorBytesLength = 0;
+
         public byte[] PixelData { get; set; }
+
         public byte[] PaletteData { get; set; }
+
+        public Sprite()
+        {
+            Version = 1;
+            CheckSum = 0xFFFF0000;
+            SpriteDataLength = 0x7000;
+            PaletteDataLength = 0x78;
+            DisplayText = "Unknown";
+            Author = "Unknown";
+            PixelData = new byte[SpriteDataLength];
+            PaletteData = new byte[PaletteDataLength];
+        }
 
         public Sprite(byte[] rawData)
         {
-            if(false == IsZSprite(rawData))
+            if (rawData.Length == 0x7078)
             {
-                throw new Exception("Invalid sprite file");
+                // old headerless sprite file
+                Version = 0;
+                CheckSum = 0;
+                SpriteDataLength = 0x7000;
+                SpriteDataOffset = 0;
+                PaletteDataLength = 0x78;
+                PaletteDataOffset = 0x7000;
+                DisplayText = "";
+                Author = "";
+                PixelData = new byte[SpriteDataLength];
+                Array.Copy(rawData, PixelData, SpriteDataLength);
+                PaletteData = new byte[PaletteDataLength];
+                Array.Copy(rawData, PaletteDataOffset, PaletteData, 0, PaletteDataLength);
+                return;
             }
 
-            Version = rawData[4];
+            if(rawData.Length < headerLength + versionLength + checksumLength + spriteDataOffsetLength + spriteDataLengthLength + paletteDataOffsetLength + paletteDataLengthLength)
+            {
+                throw new Exception("Invalid sprite file. Too short.");
+            }
+            if (false == IsZSprite(rawData))
+            {
+                throw new Exception("Invalid sprite file. Wrong header.");
+            }
+
+            Version = rawData[versionOffset];
+
+            CheckSum = bytesToUInt(rawData, checksumOffset);
+
+            SpriteDataOffset = bytesToUInt(rawData, spriteDataOffsetOffset);
+            SpriteDataLength = bytesToUShort(rawData, spriteDataLengthOffset);
+
+            PaletteDataOffset = bytesToUInt(rawData, paletteDataOffsetOffset);
+            PaletteDataLength = bytesToUShort(rawData, paletteDataLengthOffset);
+
+            Array.Copy(rawData, reservedOffset, Reserved, 0, reservedLength);
+
+            uint endOfDisplay = GetNullTerminatorLocation(rawData, displayTextOffset);
+            uint displayLength = endOfDisplay - displayTextOffset;
+            if (displayLength > 0)
+            {
+                byte[] displayTextBytes = new byte[displayLength];
+                Array.Copy(rawData, displayTextOffset, displayTextBytes, 0, displayLength);
+                DisplayText = Encoding.Unicode.GetString(displayTextBytes);
+            }
+            else
+            { 
+                DisplayText = "";
+            }
+
+            uint authorTextOffset = endOfDisplay + 2;
+            uint endOfAuthor = GetNullTerminatorLocation(rawData, authorTextOffset);
+            uint authorLength = endOfAuthor - authorTextOffset;
+            if(authorLength > 0)
+            {
+                byte[] authorTextBytes = new byte[authorLength];
+                Array.Copy(rawData, authorTextOffset, authorTextBytes, 0, authorLength);
+                Author = Encoding.Unicode.GetString(authorTextBytes);
+            }
+            else
+            {
+                Author = "";
+            }
+
+            PixelData = new byte[SpriteDataLength];
+            Array.Copy(rawData, SpriteDataOffset, PixelData, 0, SpriteDataLength);
+            PaletteData = new byte[PaletteDataLength];
+            Array.Copy(rawData, PaletteDataOffset, PaletteData, 0, PaletteDataLength);
+        }
+
+        public uint GetNullTerminatorLocation(byte[] rawData, uint offset)
+        {
+            for(uint i = offset; i < rawData.Length; i+=2)
+            {
+                if(rawData[i] == 0 && i+1 < rawData.Length && rawData[i+1] == 0)
+                {
+                    return i;
+                }
+            }
+
+            return offset;
         }
 
         public bool IsZSprite(byte[] rawData)
@@ -77,6 +202,89 @@ palette data (0x78 + ? bytes) (remember to add extra bytes for gloves)
             }
 
             return true;
+        }
+
+        public byte[] ToByteArray()
+        {
+            Version = currentVersion; // update the version
+
+            byte[] ret = new byte[headerLength + versionLength + checksumLength + spriteDataOffsetLength + spriteDataLengthLength + paletteDataOffsetLength + paletteDataLengthLength + reservedLength + displayBytesLength + authorBytesLength + SpriteDataLength + PaletteDataLength];
+
+            int i = 0;
+            ret[i++] = (byte)Header[0];
+            ret[i++] = (byte)Header[1];
+            ret[i++] = (byte)Header[2];
+            ret[i++] = (byte)Header[3];
+
+            ret[i++] = Version;
+
+            // check sum
+            byte[] checksum = BitConverter.GetBytes(CheckSum);
+            ret[i++] = checksum[0];
+            ret[i++] = checksum[1];
+            ret[i++] = checksum[2];
+            ret[i++] = checksum[3];
+
+            byte[] spriteDataOffset = BitConverter.GetBytes(SpriteDataOffset);
+            ret[i++] = spriteDataOffset[0];
+            ret[i++] = spriteDataOffset[1];
+            ret[i++] = spriteDataOffset[2];
+            ret[i++] = spriteDataOffset[3];
+
+            byte[] spriteDataLength = BitConverter.GetBytes(SpriteDataLength);
+            ret[i++] = spriteDataLength[0];
+            ret[i++] = spriteDataLength[1];
+
+            byte[] paletteDataOffset = BitConverter.GetBytes(PaletteDataOffset);
+            ret[i++] = paletteDataOffset[0];
+            ret[i++] = paletteDataOffset[1];
+            ret[i++] = paletteDataOffset[2];
+            ret[i++] = paletteDataOffset[3];
+
+            byte[] paletteDataLength = BitConverter.GetBytes(PaletteDataLength);
+            ret[i++] = paletteDataLength[0];
+            ret[i++] = paletteDataLength[1];
+
+            ret[i++] = Reserved[0];
+            ret[i++] = Reserved[1];
+            ret[i++] = Reserved[2];
+            ret[i++] = Reserved[3];
+            ret[i++] = Reserved[4];
+            ret[i++] = Reserved[5];
+            ret[i++] = Reserved[6];
+            ret[i++] = Reserved[7];
+
+            for (int x = 0; x < displayBytes.Length; x++)
+            {
+                ret[i++] = displayBytes[x];
+            }
+
+            for (int x = 0; x < authorBytes.Length; x++)
+            {
+                ret[i++] = authorBytes[x];
+            }
+
+            for (int x=0; x < PixelData.Length; x++)
+            {
+                ret[i++] = PixelData[x];
+            }
+
+            for (int x = 0; x < PaletteData.Length; x++)
+            {
+                ret[i++] = PaletteData[x];
+            }
+
+            return ret;
+        }
+
+        uint bytesToUInt(byte[] bytes, int offset)
+        {
+            return BitConverter.ToUInt32(bytes, offset);
+        }
+
+        ushort bytesToUShort(byte[] bytes, int offset)
+        {
+            return BitConverter.ToUInt16(bytes, offset);
         }
     }
 }
