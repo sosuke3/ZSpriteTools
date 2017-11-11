9e1f3db2cc39828e283c31d772dd7bad14e10f26
Josh Neta <joshneta.github@gmail.com>
2017-11-11 05:58:27 -0600

    Let's see if Appveyor can do a better job

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Sprite Tools for Zelda: ALttP [![Build Status](https://travis-ci.org/sosuke3/ZSpriteTools.svg?branch=master)](https://travis-ci.org/sosuke3/ZSpriteTools)
+# Sprite Tools for Zelda: ALttP [![Build status](https://ci.appveyor.com/api/projects/status/b2nk76ux52lavixr?svg=true)](https://ci.appveyor.com/project/JoshNeta/zspritetools)
 
 This project contains a library for working with the ZSPR file format used with ALttP randomizer. You will find a sample executable project with examples of how to use the library (and it's useful as a tool for spriters).
--- SpriteLibrary.Tests/Properties/AssemblyInfo.cs
+++ SpriteLibrary.Tests/Properties/AssemblyInfo.cs
@@ -9,6 +9,12 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyProduct("SpriteLibrary.Tests")]
 
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
@@ -16,3 +22,16 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("9abbd6a9-5f95-442a-88db-3fc1ebf374a7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.8")]
+[assembly: AssemblyFileVersion("1.0.8")]
+[assembly: AssemblyInformationalVersion("1.0.8")]
