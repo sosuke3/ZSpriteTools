c49b24ee507fe5a4466ea1d522a98c4ab3b124b3
Josh Neta <joshneta.github@gmail.com>
2017-11-10 21:42:29 -0600

    Auto build version info. Let's see how travis deals with this.

--- SpriteLibrary/Properties/AssemblyInfo.cs
+++ SpriteLibrary/Properties/AssemblyInfo.cs
@@ -7,12 +7,7 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyTitle("SpriteLibrary")]
 [assembly: AssemblyDescription("ALttP Sprite Library")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SpriteLibrary")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -21,15 +16,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("d70b706f-5023-4e39-9bb7-206457305d3f")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.*")]
